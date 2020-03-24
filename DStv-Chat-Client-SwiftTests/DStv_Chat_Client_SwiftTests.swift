4fd869c6aa7087acd28d269ab9844219c2f2b783
Kudzai <kudzaimhou@gmail.com>
2020-03-24 17:36:06 +0200

    Initial Commit

---
+++ DStv-Chat-Client-SwiftTests/DStv_Chat_Client_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  DStv_Chat_Client_SwiftTests.swift
+//  DStv-Chat-Client-SwiftTests
+//
+//  Created by Kudzaiishe Mhou on 2020/03/24.
+//  Copyright © 2020 Kudzaiishe Mhou. All rights reserved.
+//
+
+import XCTest
+@testable import DStv_Chat_Client_Swift
+
+class DStv_Chat_Client_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
